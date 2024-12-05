bf11e82818c2767c6f4713aadc17cf97183a80b6
shadow62x <quyfos@gmail.com>
2024-12-06 00:02:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# CatBypasser
+
+CatBypasser... in python???
+
+Yes, catbypasser is now available on python, with an open-sourced method.
+
+## Installation
+
+```bash
+pip install catbypasser
+```
+
+## Usage
+
+```python
+from catbypasser import translate
+
+text = "your text"
+bypassed = translate(text)
+print(bypasses)  # prints the bypassed sentence.
+```
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="catbypasser",
+    version="1.0.0",
+    description="A simple bypasser",
+    author="shadow62",
+    author_email="strippersex8@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
